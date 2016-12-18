73a2ce2f7754a4303f8ac537f6f7dd250481474d
gongfuxiang <fuxiang.gong@qq.com>
2016-12-18 23:09:09 +0800

    权限完成

--- Public/Admin/Js/Init.js
+++ Public/Admin/Js/Init.js
@@ -1,13 +1,10 @@
-$(function()
+// 全屏操作
+$('#admin-fullscreen').on('click', function()
 {
-	// 全屏操作
-	$('#admin-fullscreen').on('click', function()
-	{
-		$.AMUI.fullscreen.toggle();
-	});
-	$(document).on($.AMUI.fullscreen.raw.fullscreenchange, function()
-	{
-		$tag = $('.admin-fulltext');
-		$tag.text($.AMUI.fullscreen.isFullscreen ? $tag.attr('fulltext-exit') : $tag.attr('fulltext-open'));
-	});
+	$.AMUI.fullscreen.toggle();
+});
+$(document).on($.AMUI.fullscreen.raw.fullscreenchange, function()
+{
+	$tag = $('.admin-fulltext');
+	$tag.text($.AMUI.fullscreen.isFullscreen ? $tag.attr('fulltext-exit') : $tag.attr('fulltext-open'));
 });
