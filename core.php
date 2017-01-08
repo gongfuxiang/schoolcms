9da869b410614a65737264aef1ac063237bbb0fe
gongfuxiang <fuxiang.gong@qq.com>
2017-01-08 18:42:03 +0800

    a

--- core.php
+++ core.php
@@ -24,8 +24,8 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG', True);
 
-// 分之模式
-define('APP_STATUS', 'develop');
+// 分之模式,master,develop,test,debug
+define('APP_STATUS', 'master');
 
 /* 定义系统目录分隔符 */
 define('DS', DIRECTORY_SEPARATOR);
