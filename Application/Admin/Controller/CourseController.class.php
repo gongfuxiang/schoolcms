9e9d96cfc9e5555cf0f945753c82c4463bfedf89
gongfuxiang <fuxiang.gong@qq.com>
2017-09-05 21:58:21 +0800

    课程管理分页bug修复

--- Application/Admin/Controller/CourseController.class.php
+++ Application/Admin/Controller/CourseController.class.php
@@ -55,7 +55,7 @@ class CourseController extends CommonController
 				'number'	=>	$number,
 				'total'		=>	$total,
 				'where'		=>	$param,
-				'url'		=>	U('Admin/Fraction/Index'),
+				'url'		=>	U('Admin/Course/Index'),
 			);
 		$page = new \My\Page($page_param);
 
